c524e8deb29bc2d3367cbc20fce9a422781e1225
mgm21 <mgm18@ic.ac.uk>
2023-12-03 18:12:36 +0000

    Improved project hygiene by implementing Assistant class

---
+++ assistant.py
@@ -0,0 +1,52 @@
+import openai
+from openai import OpenAI
+from pydub import AudioSegment
+import os
+
+
+class Assistant:
+    def __init__(self,
+                 instruction_str_system,
+                 gpt_model,
+                 audio_model,
+                 voice,
+                 name):
+        self.instruction_str_system = instruction_str_system
+        self.gpt_model = gpt_model
+        self.audio_model = audio_model
+        self.voice = voice
+        self.messages = [self._convert_content_str_to_dict(instruction_str_system, role="system")]
+        self.current_str = ""
+        self.client = OpenAI()
+        self.name = name
+
+        self.audio_path = "./audio_files"
+        if not os.path.isdir("./audio_files"):
+            os.mkdir("./audio_files")
+
+        self.curr_audio_response_location = None
+
+    def produce_response_to_msg(self, msg, turn):
+        # Append msg from other character/agent to list of messages
+        self.messages += [self._convert_content_str_to_dict(content=msg, role="user")]
+
+        # Fetch response from remote GPT model
+        response = self.client.chat.completions.create(model=self.gpt_model, messages=self.messages)
+        response_content_str = response.choices[0].message.content
+        self.messages += [self._convert_content_str_to_dict(content=response_content_str, role="assistant")]
+        print(f"{self.name}: {response_content_str}")
+
+        # Produce and save audio response as mp3
+        audio_response = openai.audio.speech.create(model=self.audio_model,
+                                                    voice=self.voice,
+                                                    input=response_content_str,
+                                                    speed=1)
+        self.curr_audio_response_location = f"{self.audio_path}/{turn}-{self.name}"
+        audio_response.stream_to_file(f"{self.curr_audio_response_location}.mp3")
+
+        # Convert and save audio response to and as wav
+        mp3_sound = AudioSegment.from_mp3(f"{self.curr_audio_response_location}.mp3")
+        mp3_sound.export(f"{self.curr_audio_response_location}.wav", format="wav")
+
+    def _convert_content_str_to_dict(self, content, role):
+        return {"role": role, "content": content}
--- main.py
+++ main.py
@@ -1,64 +1,44 @@
-import openai
-from openai import OpenAI
 import simpleaudio as sa
-from pydub import AudioSegment
-import os
+from assistant import Assistant
 
 # Parameters
-instruction_str_system_1 = "You believe the earth is flat. You speak in rhymes. Your answers are short."
-instruction_str_system_2 = "You believe the earth is round. You speak in rhymes. Your answers are short."
-initial_user_prompt = "Is the earth round?"
-num_dialogue_turns = 3
+instruction_str_system_1 = "You are a debater who loves AI. You speak in rhymes. You answer in under 2 sentences."
+instruction_str_system_2 = "You are a debater who dislikes AI. You speak in rhymes. You answer in under 2 sentences."
+initial_user_prompt = "Do you love AI?"
+num_dialogue_turns = 5
 gpt_model_1 = "gpt-3.5-turbo"
 gpt_model_2 = "gpt-3.5-turbo"
 audio_model_1 = "tts-1"
 audio_model_2 = "tts-1"
-voice_1 = "alloy"
-voice_2 = "onyx"
-
+voice_1 = "nova"
+voice_2 = "echo"
 
 ########################################################################################################################
-def content_str_to_dict(content, role):
-    return {"role": role, "content": content}
+assistant1 = Assistant(instruction_str_system=instruction_str_system_1,
+                       gpt_model=gpt_model_1,
+                       audio_model=audio_model_1,
+                       voice=voice_1,
+                       name="assistant_1")
 
+assistant2 = Assistant(instruction_str_system=instruction_str_system_2,
+                       gpt_model=gpt_model_2,
+                       audio_model=audio_model_2,
+                       voice=voice_2,
+                       name="assistant_2")
 
-client = OpenAI()
-messages_1 = [content_str_to_dict(instruction_str_system_1, role="system")]
-messages_2 = [content_str_to_dict(instruction_str_system_2, role="system")]
-assistant_2_str = initial_user_prompt
-audio_path = "./audio_files"
-if not os.path.isdir("./audio_files"):
-    os.mkdir("./audio_files")
+assistant2.current_str = initial_user_prompt
 
 for turn in range(num_dialogue_turns):
-    messages_1 += [content_str_to_dict(assistant_2_str, role="user")]
-    response_1 = client.chat.completions.create(model=gpt_model_1, messages=messages_1)
-    assistant_1_str = response_1.choices[0].message.content
-    messages_1 += [content_str_to_dict(assistant_1_str, role="assistant")]
-    print(assistant_1_str)
-    response = openai.audio.speech.create(model=audio_model_1, voice=voice_1, input=assistant_1_str, speed=1)
-    response.stream_to_file(f"{audio_path}/{turn}-ast1.mp3")
-
-    # Create a wav file from the mp3
-    sound = AudioSegment.from_mp3(f"{audio_path}/{turn}-ast1.mp3")
-    sound.export(f"{audio_path}/{turn}-ast1.wav", format="wav")
-    wave_obj_1 = sa.WaveObject.from_wave_file(f"{audio_path}/{turn}-ast1.wav")
+    # Produce and speak assistant 1 response
+    assistant1.produce_response_to_msg(msg=assistant2.current_str, turn=turn)
+    wave_obj_1 = sa.WaveObject.from_wave_file(f"{assistant1.curr_audio_response_location}.wav")
     if turn > 0:
         play_obj_2.wait_done()
     play_obj_1 = wave_obj_1.play()
 
-    messages_2 += [content_str_to_dict(assistant_1_str, role="user")]
-    response_2 = client.chat.completions.create(model=gpt_model_2, messages=messages_2)
-    assistant_2_str = response_2.choices[0].message.content
-    messages_2 += [content_str_to_dict(assistant_2_str, role="assistant")]
-    print(assistant_2_str)
-    response = openai.audio.speech.create(model=audio_model_2, voice=voice_2, input=assistant_2_str, speed=1)
-    response.stream_to_file(f"{audio_path}/{turn}-ast2.mp3")
-
-    # Create a wav file from the mp3
-    sound = AudioSegment.from_mp3(f"{audio_path}/{turn}-ast2.mp3")
-    sound.export(f"{audio_path}/{turn}-ast2.wav", format="wav")
-    wave_obj_2 = sa.WaveObject.from_wave_file(f"{audio_path}/{turn}-ast2.wav")
+    # Produce and speak assistant 2 response
+    assistant2.produce_response_to_msg(msg=assistant1.current_str, turn=turn)
+    wave_obj_2 = sa.WaveObject.from_wave_file(f"{assistant2.curr_audio_response_location}.wav")
     play_obj_1.wait_done()
     play_obj_2 = wave_obj_2.play()
 
