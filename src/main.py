9b2aef1c9887e19ff0af8f9bcc7ab0e5095282f0
mgm21 <mgm18@ic.ac.uk>
2023-12-17 19:26:28 +0100

    Implemented function to save experiment parameters to text file

--- src/main.py
+++ src/main.py
@@ -2,37 +2,41 @@ from assistant import Assistant
 from conversation_moderator import ConversationModerator
 import utils
 
-# Parameters
-instruction_str_system_1 = "You are a debater who loves AI. You speak in rhymes. You answer in under 2 sentences."
-instruction_str_system_2 = "You are a debater who dislikes AI. You speak in rhymes. You answer in under 2 sentences."
-initial_user_prompt = "Do you love AI?"
-num_dialogue_turns = 2
-gpt_model_1 = "gpt-3.5-turbo"
-gpt_model_2 = "gpt-3.5-turbo"
-audio_model_1 = "tts-1"
-audio_model_2 = "tts-1"
-voice_1 = "nova"
-voice_2 = "echo"
+parameters = {
+    "instruction_str_system_1": "You are a debater who loves AI. You speak in rhymes. You answer in under 2 sentences.",
+    "instruction_str_system_2": "You are a debater who dislikes AI. You speak in rhymes. You answer in under 2 "
+                                "sentences.",
+    "initial_user_prompt": "Do you love AI?",
+    "num_dialogue_turns": 2,
+    "gpt_model_1": "gpt-3.5-turbo",
+    "gpt_model_2": "gpt-3.5-turbo",
+    "audio_model_1": "tts-1",
+    "audio_model_2": "tts-1",
+    "voice_1": "nova",
+    "voice_2": "echo",
+    "name_1": "Assistant 1",
+    "name_2": "Assistant 2"
+}
 
-results_folder_path = utils.create_timestamped_results_folder()
+results_folder_path = utils.configure_results_folder(**parameters)
 
-assistant1 = Assistant(instruction_str_system=instruction_str_system_1,
-                       gpt_model=gpt_model_1,
-                       audio_model=audio_model_1,
-                       voice=voice_1,
-                       name="assistant_1",
+assistant1 = Assistant(instruction_str_system=parameters["instruction_str_system_1"],
+                       gpt_model=parameters["gpt_model_1"],
+                       audio_model=parameters["audio_model_1"],
+                       voice=parameters["voice_1"],
+                       name=parameters["name_1"],
                        results_folder_path=results_folder_path)
 
-assistant2 = Assistant(instruction_str_system=instruction_str_system_2,
-                       gpt_model=gpt_model_2,
-                       audio_model=audio_model_2,
-                       voice=voice_2,
-                       name="assistant_2",
+assistant2 = Assistant(instruction_str_system=parameters["instruction_str_system_2"],
+                       gpt_model=parameters["gpt_model_2"],
+                       audio_model=parameters["audio_model_2"],
+                       voice=parameters["voice_2"],
+                       name=parameters["name_2"],
                        results_folder_path=results_folder_path)
 
 conversation_moderator = ConversationModerator(assistant_1=assistant1,
                                                assistant_2=assistant2,
-                                               initial_user_prompt=initial_user_prompt,
-                                               num_dialogue_turns=num_dialogue_turns)
+                                               initial_user_prompt=parameters["initial_user_prompt"],
+                                               num_dialogue_turns=parameters["num_dialogue_turns"])
 
 conversation_moderator.play_conversation()
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,12 @@
 import os
 import datetime
+import json
+
+
+def configure_results_folder(**kwargs):
+    results_folder_path = create_timestamped_results_folder()
+    create_experiment_summary_file(results_folder_path, **kwargs)
+    return results_folder_path
 
 
 def create_timestamped_results_folder():
@@ -14,3 +21,9 @@ def create_timestamped_results_folder():
         os.mkdir(results_folder_path)
 
     return results_folder_path
+
+
+def create_experiment_summary_file(results_folder_path, **kwargs):
+    # Create the file at the results_folder_path
+    with open(f'{results_folder_path}/experiment_summary.txt', 'w') as file:
+        file.write(json.dumps(kwargs, indent=2))
