2142cf000efcf19960a0660570c4de3361b77671
mgm21 <mgm18@ic.ac.uk>
2023-12-18 10:42:57 +0100

    Populated config.yaml file to store experiment parameters

--- src/main.py
+++ src/main.py
@@ -1,42 +1,31 @@
 from assistant import Assistant
 from conversation_moderator import ConversationModerator
 import utils
+import definitions
+import yaml
 
-parameters = {
-    "instruction_str_system_1": "You are a debater who loves AI. You speak in rhymes. You answer in under 2 sentences.",
-    "instruction_str_system_2": "You are a debater who dislikes AI. You speak in rhymes. You answer in under 2 "
-                                "sentences.",
-    "initial_user_prompt": "Do you love AI?",
-    "num_dialogue_turns": 2,
-    "gpt_model_1": "gpt-3.5-turbo",
-    "gpt_model_2": "gpt-3.5-turbo",
-    "audio_model_1": "tts-1",
-    "audio_model_2": "tts-1",
-    "voice_1": "nova",
-    "voice_2": "echo",
-    "name_1": "Assistant 1",
-    "name_2": "Assistant 2"
-}
+with open(definitions.CONFIG_PATH) as f:
+    PARAMS = yaml.load(f, Loader=yaml.BaseLoader)
 
-results_folder_path = utils.configure_results_folder(**parameters)
+results_folder_path = utils.configure_results_folder(**PARAMS)
 
-assistant1 = Assistant(instruction_str_system=parameters["instruction_str_system_1"],
-                       gpt_model=parameters["gpt_model_1"],
-                       audio_model=parameters["audio_model_1"],
-                       voice=parameters["voice_1"],
-                       name=parameters["name_1"],
+assistant1 = Assistant(instruction_str_system=PARAMS["instruction_str_system_1"],
+                       gpt_model=PARAMS["gpt_model_1"],
+                       audio_model=PARAMS["audio_model_1"],
+                       voice=PARAMS["voice_1"],
+                       name=PARAMS["name_1"],
                        results_folder_path=results_folder_path)
 
-assistant2 = Assistant(instruction_str_system=parameters["instruction_str_system_2"],
-                       gpt_model=parameters["gpt_model_2"],
-                       audio_model=parameters["audio_model_2"],
-                       voice=parameters["voice_2"],
-                       name=parameters["name_2"],
+assistant2 = Assistant(instruction_str_system=PARAMS["instruction_str_system_2"],
+                       gpt_model=PARAMS["gpt_model_2"],
+                       audio_model=PARAMS["audio_model_2"],
+                       voice=PARAMS["voice_2"],
+                       name=PARAMS["name_2"],
                        results_folder_path=results_folder_path)
 
 conversation_moderator = ConversationModerator(assistant_1=assistant1,
                                                assistant_2=assistant2,
-                                               initial_user_prompt=parameters["initial_user_prompt"],
-                                               num_dialogue_turns=parameters["num_dialogue_turns"])
+                                               initial_user_prompt=PARAMS["initial_user_prompt"],
+                                               num_dialogue_turns=PARAMS["num_dialogue_turns"])
 
 conversation_moderator.play_conversation()
